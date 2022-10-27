65976599948b259767b81f648be7c8a3601c2bd5
KaveCoding <teachereli@outlook.com>
2022-10-27 11:49:49 +0200

    Add project files.

---
+++ RestaurangenGit/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace RestaurangenGit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
